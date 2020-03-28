48eb87297e4cc2e47582240fade3994cdefa0c4f
Fernando Ghiberti <ghiberti@Fernandos-MacBook-Pro.local>
2020-03-28 18:03:40 -0300

    Front-End da Aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './global.css';
+import Routes from './routes';
+
+function App() { //Componente
+  
+  
+  return (
+      //<h1> Fala comigo chefe </h1> //JSX
+      //< Header title="Fala comigo chefe" />
+      //<Header> Semana Omnistack </Header>
+     <Routes />
+  );
+
+}
+
+export default App;
